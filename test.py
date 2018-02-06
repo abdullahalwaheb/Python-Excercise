56eefc349c98a237b8d99775a167240432b6340c
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-06 13:15:10 -0600

    more practice added

--- test.py
+++ test.py
@@ -1,10 +1,63 @@
-name = "test"
-print(name);
 
-answer = 1
-while answer != 2:
-  answer = input("Say 2: ")
+# !!! uncomment the block you'd want to use !!!
 
-print("Cheese")
+#---------------------------What day is it-----------------------------
 
-print("I am a new line")
+# day = int(input("day (0-2)? "))
+# if day == 0:
+#   print("It is Sunday")
+# elif day == 1:
+#   print("It is Monday")
+# else:
+#   print("It is Funday")
+
+
+#----------------------Temperature calculator-------------------------
+
+# temp = float(input("What is today's temperature? "))
+# celsius = (temp - 30) / 2
+# print("Today's temperature is ", temp," F and ", celsius," in celsius")
+
+
+#---------------------------Tip Calculator----------------------------
+# bill = 0
+# tip = 0.0
+# total = 0
+# rate = ""
+
+# def calcTotal():
+#   bill = float(input("What is your total?\n$"))
+#   rate = input("How would you rate us? Good,Fair or Crap?\n")
+#   if (rate == "good" or rate == "Good"):
+#     tip = 0.20
+#   elif (rate == "fair" or rate == "Fair"):
+#     tip = 0.10
+#   elif (rate == "crap" or rate == "Crap"):
+#     tip = 0.05
+#   else:
+#     print("Invalid rating option")
+
+#   total = bill + bill * tip
+#   print("Your total is: $",total)
+
+# calcTotal()
+
+
+#--------------------------Loop Practice------------------------------
+
+# i = 0
+# while i < 15:
+#   i += 1
+#   print(i)
+
+#--------------------------How many coins?------------------------------
+
+# coins = 0
+
+# answer = input("Do want some coins? ")
+# while (answer == "yes"):
+#   coins += 1
+#   answer = input("You have %s coin, want another one? " % coins)
+#   if (answer == "no"):
+#     print("whatever!!! I am taking'em all back BYE")
+#     break
