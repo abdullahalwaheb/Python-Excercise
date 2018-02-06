3d80e17a9dea8de744a22d5eea0e3194a10218e8
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-06 17:25:34 -0600

    modified

--- stringPractice.py
+++ stringPractice.py
@@ -2,4 +2,15 @@ name = "mark"
 
 print(name.upper())
 print(name.capitalize())
-#reverse here
+print(name[::-1])
+
+string = input('Please enter a word: ').upper()
+
+string = string.replace('A', '4')
+string = string.replace('E', '3')
+string = string.replace('G', '6')
+string = string.replace('I', '1')
+string = string.replace('O', '0')
+string = string.replace('S', '5')
+string = string.replace('T', '7')
+
