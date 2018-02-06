8b8cb0f130863f5eab473afdcf432b7099620b1d
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-06 13:34:46 -0600

    modified

--- test.py
+++ test.py
@@ -2,7 +2,6 @@
 # !!! uncomment the block you'd want to use !!!
 
 #---------------------------What day is it-----------------------------
-
 # day = int(input("day (0-2)? "))
 # if day == 0:
 #   print("It is Sunday")
@@ -13,55 +12,51 @@
 
 
 #----------------------Temperature calculator-------------------------
-
 # temp = float(input("What is today's temperature? "))
 # celsius = (temp - 30) / 2
 # print("Today's temperature is ", temp," F and ", celsius," in celsius")
 
 
 #---------------------------Tip Calculator----------------------------
+# bill = 0
+# tip = 0.0
+# total = 0
+# rate = ""
 
-bill = 0
-tip = 0.0
-total = 0
-rate = ""
+# def calcTotal():
+#   while True:
+#     try:
+#       bill = float(input("What is your total?\n$"))
+#       rate = input("How would you rate us? Good,Fair or Crap?\n")
+#       if (rate == "good" or rate == "Good"):
+#         tip = 0.20
+#       elif (rate == "fair" or rate == "Fair"):
+#         tip = 0.10
+#       elif (rate == "crap" or rate == "Crap"):
+#         tip = 0.05
+#       else:
+#         print("invalid value")
 
-def calcTotal():
-  while True:
-    try:
-      bill = float(input("What is your total?\n$"))
-      rate = input("How would you rate us? Good,Fair or Crap?\n")
-      if (rate == "good" or rate == "Good"):
-        tip = 0.20
-      elif (rate == "fair" or rate == "Fair"):
-        tip = 0.10
-      elif (rate == "crap" or rate == "Crap"):
-        tip = 0.05
-      else:
-        print("invalid value")
+#       total = bill + bill * tip
+#       print("Your total is: $",total)
 
-      total = bill + bill * tip
-      print("Your total is: $",total)
-      
-    except ValueError:
-      print("Your bill has to be a number")
-    else:
-      return bill
-      break
+#     except ValueError:
+#       print("Your bill has to be a number")
+#     else:
+#       return bill
+#       break
 
 
-calcTotal()
+# calcTotal()
 
 
 #--------------------------Loop Practice------------------------------
-
 # i = 0
 # while i < 15:
 #   i += 1
 #   print(i)
 
 #--------------------------How many coins?------------------------------
-
 # coins = 0
 
 # answer = input("Do want some coins? ")
