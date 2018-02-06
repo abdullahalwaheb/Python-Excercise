193ee1d5e35c673abd91353114e16b91de530ce4
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-05 19:07:27 -0600

    first commit thru cli

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-answer = ''
-while answer not in ['y' , 'n']:
-    answer = input('Say y or n: ')
-    answer = answer.lower()
+name = "test"
+print(name);
+
+answer = 1
+while answer != 2:
+  answer = input("Say 2: ")
 
 print("Cheese")
