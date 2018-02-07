5a534c70536ceac68fbf12a83fab7dad793414b3
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-07 15:38:41 -0600

    list practice modified

--- listPractice.py
+++ listPractice.py
@@ -1,34 +1,41 @@
+
 #declaring variables
 myList = [-1,-2,-5,1,2,3,4,5,6]
 newList = []
 
-#Sum of numbers in a list
+#-------------------Sum of numbers in a list---------------------
 print("The sum of numbers in the list is: ", sum(myList))
 
-#Largest number in a list
+
+#-------------------Largest number in a list---------------------
 print("The largest number in the list is: ", max(myList))
 
-#Smallest number in a list
+
+#-------------------Smallest number in a list---------------------
 print("The smallest number in the list is: ", min(myList))
 
-#Even numbers in a list
+
+#-------------------Even numbers in a list---------------------
 print("The even numbers in this list are:")
 for i in myList:
     if (i%2 == 0):
         print(i)
 
-#Positive numbers in a list
+
+#-------------------Positive numbers in a list---------------------
 print("The positive numbers in this list are:")
 for i in myList:
     if (i > 0):
         print(i)
 
-#Positive numbers2 in a new list
+
+#-------------------Positive numbers2 in a new list---------------------
 print("The positive numbers from list one to two are:")
 for i in myList:
     if (i > 0):
         newList.append(i)
 print(newList)
+
 
 #more varialbes for multiplication --------------
 list1 = [1,2,3]
@@ -37,14 +44,27 @@ vectorList = []
 additionList = []
 
 
-#multiplication of vector
+#---------------------multiplication of vector---------------------
 print("The new multiplication list is:")
 for k in range(0, len(list1)):
     vectorList.append(list1[k]*list2[k])
 print(vectorList)
 
-#addition of two lists
+
+#---------------------addition of two lists---------------------
 print("The addition list is:")
 for k in range(0, len(list1)):
     additionList.append(list1[k]+list2[k])
 print(additionList)
+
+
+#---------------------due up---------------------
+oldList = ["Mark","Mark","John","John","Rick"]
+tempString = ""
+newList = []
+
+for i in range(0,len(oldList)):
+    if (tempString != oldList[i]):
+        tempString = oldList[i]
+        newList.append(tempString)
+print(newList)
