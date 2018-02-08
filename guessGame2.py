c293ac44d056b9c0115b4425760ae548338e67b5
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-08 09:06:56 -0600

    new file

---
+++ guessGame2.py
@@ -0,0 +1,24 @@
+import random
+
+randomNum = random.randint(1,100)
+print("Is the number in your head:", randomNum)
+
+def rounder():
+    randomNum = randomNum - 10
+
+while True:
+    try:
+        choice = int(input("1.Higher\n2.Less\n3.You got it!)\n"))
+        if(choice==1):
+            print("too high")
+            break
+        elif (choice==2):
+            print("too low")
+            break
+        elif (choice==3):
+            print("thats it")
+            break
+    except ValueError:
+        print("invalid option")
+
+
