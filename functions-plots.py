b28c4dcdf5eabb90601b139be4328c330fb0deb9
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-08 11:06:40 -0600

    modified

---
+++ functions-plots.py
@@ -0,0 +1,47 @@
+#----------------------uncomment block to use---------------------
+
+# #----------------------Hello Function---------------------
+# def hello(name):
+#     print("Hello", name)
+
+# hello("John")
+
+
+# #----------------------X+1---------------------
+# from matplotlib import pyplot
+
+# def f(x):
+#     return(x+1)
+
+# def run():
+#     xs = list(range(-3, 4))
+#     ys = []
+#     for x in xs:
+#         ys.append(f(x))
+
+#     pyplot.plot(xs, ys)
+#     pyplot.show()
+
+# if __name__ == "__main__":
+#     run()
+
+# #---------------------Square of X---------------------
+# from matplotlib import pyplot
+
+# def f(x):
+#     return(x*x)
+
+# def run():
+#     xs = list(range(-100, 100))
+#     ys = []
+#     for x in xs:
+#         ys.append(f(x))
+
+#     pyplot.plot(xs, ys)
+#     pyplot.show()
+
+# if __name__ == "__main__":
+#     run()
+
+
+#---------------------Odd or even---------------------
