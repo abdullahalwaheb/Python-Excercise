e2650a0729eaa7daf799421bf6d0a705c9f5d088
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-09 11:30:16 -0600

    added files

--- interview-challenge-1/algorithm1.py
+++ interview-challenge-1/algorithm1.py
@@ -1,8 +1,14 @@
-numbers = list(range(1,20))
+numbers = list(range(1,101))
 
-for i in numbers:
-    if i%3==0:
+for i in range(1,101):
+    if (i%3 == 0) and (i%5==0):
+        numbers.insert(i,"FizzBuzz")
         numbers.remove(i)
+    elif (i%3)==0:
         numbers.insert(i,"Fizz")
+        numbers.remove(i)
+    elif (i%5)==0:
+        numbers.insert(i,"Buzz")
+        numbers.remove(i)
 
 print(numbers)
---
+++ interview-challenge-1/algorithm2.py
@@ -0,0 +1,7 @@
+newList = []
+
+for i in range(1,1000):
+    if (i%5==0) or (i%3==0):
+        newList.append(i)
+
+print(sum(newList))
