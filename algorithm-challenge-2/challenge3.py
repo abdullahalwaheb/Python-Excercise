78d077644f281525aa2addc13950e62f4b60a870
Abdullah <abdullah.alwaheb@gmail.com>
2018-02-16 15:28:45 -0600

    algorithm updated

--- algorithm-challenge-2/challenge3.py
+++ algorithm-challenge-2/challenge3.py
@@ -1,27 +1,27 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+#variables
 result = []
-num = 2515
-i = 1
+num = 1
+denominator = 1
 
-def repeat():
-    while (num % i == 0):
-        result.append(num)
-        i+= 1
-    else:
+#algorithm
+while True:  
+    if (num % denominator == 0):
+        #does not matter what number to append, this is just to fill the length up to 20
+        result.append("0")
+        denominator += 1
+    elif (num % denominator != 0):
         result = []
         num += 1
-        i = 1
-        repeat()
-        
+        denominator = 1
+    if (len(result) == 20):
+        break
 
-if (len(result) == 10):
-    result = []
-    result.append(num)
-else:
-    print("the number is not found")
-
-repeat()
+#The number should be 232792560
 print(num)
-print(result)
+
+#validate the result
+for i in range(1,21):
+    print(num%i)
